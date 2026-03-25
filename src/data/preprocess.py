7959a9a6bdf6f5fc1ca7bada3c7f91df71cc3ca7
Ryqn0 <dungryan0@gmail.com>
2026-03-25 15:02:40 +0100

    Added load_data.py and preprocess.py

---
+++ src/data/preprocess.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+def preprocess_data(data, target_column = 'Churn'):
+    """
+    Preprocess the data by handling missing values and encoding categorical variables.
+
+    Parameters:
+    data (pd.DataFrame): The input DataFrame to preprocess.
+
+    Returns:
+    pd.DataFrame: A preprocessed DataFrame ready for analysis or modeling.
+    """
+
+    for column in data.columns:
+        if data[column].dtype == 'object':
+            data[column] = data[column].fillna(data[column].mode()[0]) # Fill missing categorical values with the mode
+        else:
+            if column == "SeniorCitizen":
+                data[column] = data[column].fillna(data[column].mode()[0]) # Fill missing values in SeniorCitizen with the mode
+                data[column] = data[column].map({0: 'No', 1: 'Yes'}) # Map 0 and 1 to 'No' and 'Yes' respectively
+            else:
+                data[column] = data[column].fillna(data[column].median()) # Fill missing numerical values with the median
+    
+    if target_column in data.columns:
+        data[target_column] = data[target_column].map({'No': 0, 'Yes': 1}) # Map target variable to binary values
+
+    categorical_cols = data.select_dtypes(include="object").columns.difference([target_column])
+    
+    # Encode categorical variables using one-hot encoding
+    data_encoded = pd.get_dummies(data, columns=categorical_cols, drop_first=False)
+    
+    return data_encoded
